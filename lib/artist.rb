b70edb74a5437e03a5fddd29412686557d6b42ef
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-03 13:59:34 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,13 @@
-require 'pry'
+#require 'pry'
 
 class Artist
-  extend Memorable 
-
   attr_accessor :name
   attr_reader :songs
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
   @@artists = []
 
@@ -21,9 +24,9 @@ class Artist
     @@artists
   end
 
-  #def self.reset_all
-    #self.all.clear
-  #end
+  def self.reset_all
+    self.all.clear
+  end
 
   #def self.count
     #self.all.count
@@ -31,14 +34,14 @@ class Artist
 
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+    #name.downcase.gsub(' ', '-')
+  #end
 end
